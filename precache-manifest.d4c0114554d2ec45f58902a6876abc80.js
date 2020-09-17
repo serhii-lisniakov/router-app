54aa9993cf95656bc2151908562e02df7f3a5c01
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-17 09:38:33 +0300

    Updates

---
+++ precache-manifest.d4c0114554d2ec45f58902a6876abc80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e26e3b9e8f262099c476f5b4e27ce54",
+    "url": "/router-app/index.html"
+  },
+  {
+    "revision": "1e453505792988df7d02",
+    "url": "/router-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38eee32e27746a2489d1",
+    "url": "/router-app/static/js/2.3bcb3254.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/router-app/static/js/2.3bcb3254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e453505792988df7d02",
+    "url": "/router-app/static/js/main.3e861860.chunk.js"
+  },
+  {
+    "revision": "a656d0ed212ed90dd53e",
+    "url": "/router-app/static/js/runtime-main.7970c8d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/router-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
