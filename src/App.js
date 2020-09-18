ca9f393a464c35a3567de4d089883a50adfe6975
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-18 10:23:08 +0300

    router app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom
 import Header from './components/Header/Header'
 import Page from './page/Page'
 import './App.css';
+import { HomePage, PostsPage, FotosPage, ContactsPage, ErrorPage} from './page/Page'
 
 function App() {
   return (
     <>
-      
         <Router basename={'/router-app'}>
           <Header/>
             <Switch>
-                <Route path="/" exact render={() => <Redirect to="/home"/>}/>
-                <Route path={'/home'} render={() => <Page class='page page-home'/>} />
-                <Route path={'/posts'} render={() => <Page class='page page-posts'/>} />
-                <Route path={'/fotos'} render={() => <Page class='page page-fotos'/>} />
-                <Route path={'/contacts'} render={() => <Page class='page page-contacts'/>} />
-                <Route component={() => <Page class='page page-error'/>} />
+                <Route path="/" exact component={HomePage}/>
+                <Route path={'/home'} component={HomePage}/>
+                <Route path={'/posts'} component={PostsPage}/>
+                <Route path={'/fotos'} component={FotosPage}/>
+                <Route path={'/contacts'} component={ContactsPage}/>
+                <Route component={() => <ErrorPage />} />
             </Switch>
         </Router>
-
     </>
   );
 }
